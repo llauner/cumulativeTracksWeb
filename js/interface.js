a6d95685f6159538daf864073adebf990082b578
Ludovic Launer <ludovic.launer@wika.com>
2020-04-07 10:26:05 +0200

    Add measuring tools, spinner

--- js/interface.js
+++ js/interface.js
@@ -1,10 +1,18 @@
 // ----- Event handlers -----
-    // --- Show / hide airspace ---
-    $('#switch-airspace').on('change',
-    function () {
-        var show = $('#switch-airspace').is(':checked');
-        isAirspaceShown = show;
-        showHideAirspace(isAirspaceShown);
-	});
+// --- Show / hide airspace ---
+$('#switch-airspace').on('change',
+function () {
+    var show = $('#switch-airspace').is(':checked');
+    isAirspaceShown = show;
+    showHideAirspace(isAirspaceShown);
+});
+
+// --- Show / hide measuring tools ---
+$('#chk-show-measuring-tools').on('change',
+function () {
+    var show = $('#chk-show-measuring-tools').is(':checked');
+    showHideMeasuringTool(show)
+});
 	
-	
+
+
